b58d54ade7719f25112d467f2dd00876887398fd
iameaszy <easyclick05@gmail.com>
2018-08-10 22:54:40 +0100

    committed

--- stories/index.js
+++ stories/index.js
@@ -1,20 +1,28 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import SignInForm from "../src/components/sign-in-form";
-import { Button } from "../src/components/button";
-import store from "../src/index";
-import { Provider } from "react-redux";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import SignInForm from '../src/components/sign-in-form';
+import LogInForm from '../src/components/login';
+import store from '../src/index';
+import App from '../src/App';
+import { Provider } from 'react-redux';
 
-let stories = storiesOf("Register form", module);
-stories.addDecorator(getStory => {
-  return (
-    <Provider store={store}>
-      <div>{getStory()}</div>
-    </Provider>
-  );
+let stories = storiesOf('Register form', module);
+
+stories.addDecorator((story) => (
+  <Provider store={store}>
+    <div>{story()}</div>
+  </Provider>
+));
+
+stories.add('signin form', () => {
+  return <SignInForm />;
 });
 
-stories.add("with disabled button", () => {
-  return <SignInForm handleSubmit={action("hello")} />;
+stories.add('login form', () => {
+  return <LogInForm />;
+});
+
+stories.add('both login and signin form', () => {
+  return <App />;
 });
